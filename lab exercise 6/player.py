252db87d4094f533199cd43c93dcdd70a1a8e3d1
JANREY <janrey_mosuela@hotmail.co.uk>
2017-10-18 18:39:35 +0100

    Player - Exercise 6

---
+++ lab exercise 6/player.py
@@ -0,0 +1,11 @@
+from items import *
+from map import rooms
+
+inventory = [item_id, item_laptop, item_money]
+
+max_capacity = 3
+
+current_inv_mass = 2.5
+
+# Start game at the reception
+current_room = rooms["Reception"]
